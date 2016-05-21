f8c66777e8355920b689b679f17ae38b56a9a33a
Kiffin Gish <kiffin.gish@planet.nl>
2016-05-21 22:10:37 +0200

    Initial upgrade to ember-2.5.1, seems to work.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,23 +3,26 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
+    var app = new EmberApp(defaults, {
+      
+        // npm install --save-dev ember-cli-inline-content
+        inlineContent: {
+            'favicons': {
+                file: './public/assets/images/favicons/favicons.txt'
+            }
+        }
+      
+    });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+    // Bootstrap
+    app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+    app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', { destDir: 'assets' });
+    app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
-  return app.toTree();
+    // Velocity: https://github.com/julianshapiro/velocity
+    //app.import('bower_components/velocity/velocity.min.js');
+    //app.import('bower_components/velocity/velocity.ui.min.js');
+
+    return app.toTree();
 };
+
