dee404863346be7c1e67ed621a8ab654894428bd
Kiffin Gish <kiffin.gish@planet.nl>
2016-05-29 18:24:01 +0200

    Moved partials to components, much better.

--- app/components/header-banner.js
+++ app/components/header-banner.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    didInsertElement: function () {
-        Ember.$('.jumbotron').velocity("fadeIn", { duration: 1000 });
-    }
 });
