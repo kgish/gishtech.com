7f5ca02f8b442773a712765aa084f7c0e92d01e3
Kiffin Gish <kiffin.gish@planet.nl>
2016-05-30 22:56:57 +0200

    Moved partials to components, much better.

--- app/transitions.js
+++ app/transitions.js
@@ -1,48 +1,51 @@
 // http://ember-animation.github.io/liquid-fire/#/
 export default function(){
+    
+    var duration = 250;
+    
     this.transition(
         this.fromRoute('index'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
     this.transition(
         this.fromRoute('skills'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
     this.transition(
         this.fromRoute('contact'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
     this.transition(
         this.fromRoute('about'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
     this.transition(
         this.fromRoute('links'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
     this.transition(
         this.fromRoute('credits'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
     this.transition(
         this.fromRoute('repository'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
     this.transition(
         this.fromRoute('logo'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
     this.transition(
         this.fromRoute('terms-and-conditions'),
-        this.use('toLeft'),
-        this.reverse('toRight')
+        this.use('toLeft', { duration }),
+        this.reverse('toRight', { duration })
     );
 }
