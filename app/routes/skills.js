acf96e5d769f7ef05b6a751e23891643d95833cb
Kiffin Gish <kiffin.gish@planet.nl>
2016-06-02 23:18:38 +0200

    Velocity stagger skills on page load.

--- app/routes/skills.js
+++ app/routes/skills.js
@@ -1,7 +1,15 @@
+import Ember from 'ember';
 import BaseRoute from './base';
 
 export default BaseRoute.extend({
     model() {
         return this.store.findAll('skill');
+    },
+    
+    activate() {
+        this._super();
+        Ember.run.scheduleOnce('afterRender', this, function(){
+            Ember.$('.skill-item').velocity('transition.fadeIn', { stagger: 50 });
+        });
     }
 });
