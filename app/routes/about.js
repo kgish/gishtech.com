fc03fb8f054e144e372bf2a0dd12b704a36f091b
Kiffin Gish <kiffin.gish@planet.nl>
2016-06-03 14:44:44 +0200

    About now fades in also.

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,12 @@
+import Ember from 'ember';
 import BaseRoute from './base';
 
 export default BaseRoute.extend({
+
+    activate() {
+        this._super();
+        Ember.run.scheduleOnce('afterRender', this, function(){
+            Ember.$('.about-item').velocity('transition.slideUpIn', { stagger: 250, drag: true });
+        });
+    }
 });
