0c02fa5dc15db746b4f2b9bd708856d727afdd8d
Kiffin Gish <kiffin.gish@planet.nl>
2016-06-06 17:15:22 +0200

    New contact form on contacts page.

--- app/components/contact-form.js
+++ app/components/contact-form.js
@@ -3,20 +3,68 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNames: ['contact-form'],
 
-    firstname: null,
-    lastname: null,
-    email: null,
-    phone: null,
-    message: null,
-    
-    validFirstname: Ember.computed('firstname', function() { 
-        var firstname = this.get('firstname');
-        return (firstname && firstname.length > 5);
-    }),
-    validLastname: true,
-    validEmail: true,
-    validPhone: true,
-    validMessage: true,
+    messageSent: false,
 
-    dsableSend: true
+    validFirstname: Ember.computed('model.firstname', function() {
+        var firstname = this.get('model.firstname'),
+            isEmpty = Ember.isEmpty(firstname);
+        return (!isEmpty && firstname.length > 1);
+    }),
+    validLastname: Ember.computed('model.lastname', function() {
+        var lastname = this.get('model.lastname'),
+            isEmpty = Ember.isEmpty(lastname);
+        return (!isEmpty && lastname.length > 1);
+    }),
+    validEmail: Ember.computed('model.email', function(){
+        var email = this.get('model.email'),
+            re = /^.+@.+\..+$/,
+            isEmpty = Ember.isEmpty(email);
+        return (!isEmpty && re.test(email.trim()));
+    }),
+    validPhone: Ember.computed('model.phone', function(){
+        // Must contain stream of digits, spaces, '.', '+' or '-' (at least 6 digits)
+        var phone = this.get('model.phone'),
+            isEmpty = Ember.isEmpty(phone);
+        return isEmpty || ( phone.match(/\d/g) && phone.match(/\d/g).length>=6 && /^\+?[ 0-9.-]+$/.test(phone.trim()) );
+    }),
+    validMessage: Ember.computed('model.message', function(){
+        var message = this.get('model.message');
+        return message && message.length > 0 && message.split(' ').length >= 10;
+    }),
+
+     enableSend: Ember.computed.and(
+        'validFirstname',
+        'validLastname',
+        'validEmail',
+        'validPhone',
+        'validMessage'
+    ),
+    disableSend: Ember.computed.not('enableSend'),
+
+    actions: {
+        // Some fancy animation shenanigans.
+        // After clearing the fields, a success message is displayed which slowly
+        // fades out in ten seconds, after which the original (now empty) dialog
+        // will reappear.
+        sendMessage() {
+            var _this = this;
+            [
+                'firstname',
+                'lastname',
+                'email',
+                'phone',
+                'message'
+            ].forEach(function(item){
+                console.log(item + '=' + _this.get('model.'+item));
+                _this.set('model.'+item, null);
+            });
+            this.set('messageSent', true);
+            Ember.run.scheduleOnce('afterRender', this, function(){
+                Ember.$('.alert-success').velocity('fadeOut', { duration: 10000 });
+                Ember.run.later(this, function(){
+                   this.set('messageSent', false);
+                }, 10000);
+            });
+        }
+    }
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,23 @@
+import Ember from 'ember';
 import BaseRoute from './base';
 
 export default BaseRoute.extend({
+
+    contact: null,
+
+    model() {
+        var contact = Ember.Object.create({
+            firstname: null,
+            lastname: null,
+            email: null,
+            message: null
+        });
+        this.set('contact', contact);
+        return  contact;
+    },
+
+    deactivate(){
+        Ember.Object.destroy(this.get('contact'));
+        this.set('contact', null);
+    }
 });
